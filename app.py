b2af48b717b922c5379d4263843aa06aeb3c11e1
Oliver Kötter <oliverkoetter@MacBook-Pro-von-Oliver.local>
2021-01-17 01:57:45 +0100

    fixed stable run

--- app.py
+++ app.py
@@ -93,10 +93,11 @@ search_words = [
 suffix = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
 ABS_PATH = os.path.abspath(__file__) #"/app.py"
 BASE_DIR = os.path.dirname(ABS_PATH) #"/"
+
 Path(os.path.join(BASE_DIR, "uploads", suffix)).mkdir(parents=True, exist_ok=True)
+INPUT = os.path.join(BASE_DIR, "uploads", suffix)
 Path(os.path.join(BASE_DIR, "downloads", suffix)).mkdir(parents=True, exist_ok=True)
-INPUT = os.path.join(BASE_DIR, "uploads")
-OUTPUT = os.path.join(BASE_DIR, "downloads")
+OUTPUT = os.path.join(BASE_DIR, "downloads", suffix)
 
 OUTPUT_NAME = "Kopfkino"
 OUPUT_URL = ""
@@ -136,11 +137,12 @@ def upload(filename):
 		# Return 400 BAD REQUEST
 		abort(400, "no subdirectories allowed")
 
-	with open(os.path.join(INPUT, filename), "wb") as fp:
+	with open(os.path.join(INPUT, os.path.join(OUTPUT, FILENAME)), "wb") as fp:
 		fp.write(request.data)
 
 	# Return 201 CREATED
 	return "", 201
+
 
 # KOPFKINO ROUTING
 @app.route('/create/<string:data>', methods=['GET'])
@@ -148,14 +150,15 @@ def create(data):
     input = data
     print("received via GET request: ", data)
     i = 1
-    try:
-        api.search(search_words[i], page=1, results_per_page=2)
-        ph = api.get_entries()
-        dl_img(ph[3].large, FILENAME)
-        print("successfull API fetch, file was written")
-    except:
-        print("ERROR!!!! Suchwort konnte nicht gefunden werden. Platzhalter wird eingesetzt!")
-    return send_from_directory(directory=OUTPUT, filename=FILENAME, as_attachment=True)
+#    try:
+    api.search(search_words[i], page=1, results_per_page=2)
+    ph = api.get_entries()
+    print(ph)
+    dl_img(ph[0].large, os.path.join(OUTPUT, str(FILENAME + ".jpg")))
+    print("successfull API fetch, file was written")
+#    except:
+#        print("ERROR!!!! Suchwort konnte nicht gefunden werden. Platzhalter wird eingesetzt!")
+    return send_from_directory(directory=OUTPUT, filename=str(FILENAME + ".jpg"), as_attachment=True)
 
 # KOPFKINO PROCESSING
 
