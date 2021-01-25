d1b04bfd3617f9fd32de4a31324c5a352faea3d4
Oliver Kötter <oliverkoetter@MacBook-Pro-von-Oliver.local>
2021-01-25 12:47:42 +0100

    cleaned app.py + added buildpack for ghostscript (fonts) with index 2 for heroku deployment

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def overlay_text(text, t):
                        method="caption",
                        align="center",
                        fontsize=(FONTSIZE_MAIN),
-                       font=FONT
+#                       font=FONT
                        )
     overlay = overlay.set_duration(t)
     return overlay
@@ -114,11 +114,11 @@ PADDING = WIDTH_OUT * 0.1
 
 readingSpeed = 0.2
 search_words = [
-    "tree",
-    "dog",
-    "sun",
-    "wind",
-    "beach"
+    "stone",
+    "town",
+    "car",
+    "sea",
+    "flower"
 ]
 
 suffix = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
@@ -181,13 +181,8 @@ def upload(filename):
 @app.route('/create/<string:data>', methods=['GET'])
 def create(data):
     # creating instance of class Processing which bundles all data and pipeline phases
-#    file = Processing(["tree", "cat", "dog", "house", "car"], style="neutral", voiceover=False)
-#    file.text_searchwords = file.user_input # will be taking formdata
     file = Processing(user_input=data, style="neutral", voiceover=False)
     file.text_searchwords = search_words  # input NLP here
-    print(file.user_input)
-    print(file.text_searchwords)
-
     file.downloaded_items = pexels_fetch(file.text_searchwords)
     for i in range(0, len(file.downloaded_items)):
         file.footage.append(zoom(file.downloaded_items[i], file.timing[i]))
@@ -204,119 +199,3 @@ def create(data):
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
-
-'''
-##########################################################################################
-# KOPFKINO PROCESSING
-# improvised version!
-examples = ["A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so.",
-            "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by a",
-            "The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words.",
-            "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.",
-            "The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. \"What's happened to me?\"",
-            "Tons of trash have washed onto Bali's famous Kuta Beach, prompting locals to spend the first day of the new year staging a cleanup. Residents from the Badung area on the Indonesian island cleared 30 tons of marine debris from the beach, according to the state-run Antara news agency. \“Some 70% of the marine debris is plastic waste,\“ Colonel Made Mahaparta from the Udayana Regional Military Command told Antara. The trash was reportedly loaded onto trucks and transported to a landfill site.",
-            "There may have been far fewer airplanes in the skies this past year, but if you're looking ahead to future travel, you might take heed of the latest rankings of the world's safest airlines from AirlineRatings.com. AirlineRatings.com keeps tabs on 385 carriers from across the globe, measuring factors including the airlines' crash and serious incident records, and age of their aircraft. \“The challenge this year was the number of airlines that were flying, although our Top 20 safest airlines have all continued to fly or had limited cessation of flights,\“ AirlineRatings editor-in-chief Geoffrey Thomas tells CNN Travel. For 2021, the airline safety and product review website awarded Aussie airline Qantas the top spot.",
-            "The coronavirus pandemic transformed the working and social lives of millions of European adults in 2020. Children too have suffered immensely, with months of confinement only giving way to some sense of normality when schools in most countries reopened in the summer and autumn. Now even that is under threat. Students in the UK were due to return from the Christmas break on Monday. But less than a week ago, the government announced the return to school would be delayed by two weeks for almost all high schoolers and some primary (elementary) school children. Learning will move online. On Sunday, Prime Minister Boris Johnson conceded schools may need to close indefinitely.",
-            "The potential removal of President Donald Trump from office starts out more popular than any other removal process of a president in recent American history. Removing Trump from office remains quite unpopular among Republicans, however. A look across polls conducted since riots at the Capitol on Wednesday shows that a clear plurality of Americans overall want Trump out of office, even as President-elect Joe Biden is set to be inaugurated on January 20. You can see that well in an ABC News/Ipsos poll released on Sunday. The majority (56%) say Trump should be removed from office, while just 43% believe he should not be removed."
-            ]
-search_sentence = examples[len(examples)-1]
-
-print("INPUT: " + search_sentence)
-
-#NLP
-#sentencizer
-search_sentence_chopped = nltk.sent_tokenize(search_sentence)
-ch_count = []
-timing = []
-
-i = 0
-for sen in search_sentence_chopped:
-    ch_count.append(len(search_sentence_chopped[i]))
-    i += 1
-
-print(search_sentence)
-print(search_sentence_chopped)
-
-#TIMING
-i = 0
-for ch in ch_count:
-#    timing.append(round((int(ch) * readingSpeed), 2))
-    x = round((int(ch) * readingSpeed), 2)
-    if x < 5:
-        x = 5
-    timing.append(x)
-    #print(timing[i])
-    #    print(timing[i])
-    i += 1
-
-print(timing)
-
-
-#video settings
-
-#attribution to stock site
-watermark = "pictures by PEXELS.com"
-
-#actual video processing
-media = []
-jpegs = []
-footage = []
-
-#preprocess NLP --> create search queries for Pexels API
-choppedSearchwords = []
-i = 0
-for sen in search_sentence_chopped:
-    choppedSearchwords.append([])
-    sen = nltk.word_tokenize(sen)
-#    print(nltk.pos_tag(sen))
-    for p in nltk.pos_tag(sen):
-        if p[1] == ("NN" or "NNS" or "NNP" or "NNPS"):
-            choppedSearchwords[i].append(p[0])
-    i += 1
-
-for s in choppedSearchwords: print(s)
-
-
-
-
-#search on API + downloading in specified resolution --> entering in jpegs array as ready to process files
-count = 0
-for s in choppedSearchwords:
-#    print(api.get_entries())
-    try:
-        api.search(choppedSearchwords[count][0], page=1, results_per_page=5)
-        ph = api.get_entries()
-        dl_img(ph[3].large, "image"+ str(count) + ".jpg")
-        jpegs.append("image"+ str(count) + ".jpg")
-        print("successfull API fetch. file was written index: image" + str(count) + ".jpeg")
-    except:
-        print("ERROR!!!! Suchwort = --> " + str(s) + "<-- konnte nicht gefunden werden. Platzhalter wird eingesetzt!")
-        jpegs.append("error.jpg")
-    count += 1
-
-#create footage
-i = 0
-for file in jpegs:
-    img = zoom(file,timing[i])
-    img = resize_to_ouput_size(img)
-    footage.append(img)
-    i += 1
-
-#create text overlay
-overlays = []
-combined = []
-i = 0
-for ins in search_sentence_chopped:
-    a = overlay_text(ins, timing[i])
-    b = overlayAttribution(watermark, timing[i])
-    c = CompositeVideoClip([footage[i], a.crossfadein(1), b])
-    combined.append(c)
-    i += 1
-
-# EXPORT experiment
-vid = concatenate_videoclips(combined)
-vid.write_videofile(os.path.join(OUTPUT, OUTPUT_NAME),codec='libx264', fps=24)
-
-# END of improvised version!
-##########################################################################################
-'''
