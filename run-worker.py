1b434376a467006711cc1851028c2455c6461cc4
Oliver Kötter <oliverkoetter@MacBook-Pro-von-Oliver.local>
2021-02-04 01:05:00 +0100

    fixed RQ backround worker

--- run-worker.py
+++ run-worker.py
@@ -5,7 +5,9 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL')
+#redis_url = os.getenv('REDISTOGO_URL')
+redis_url = "redis://redistogo:3f1232c25d4635ab5422d0cced370d16@soapfish.redistogo.com:10834/"
+print(os.getenv("REDISTOGO_URL"))
 print(f"set Redis worker instance to {os.getenv('REDISTOGO_URL')}")
 
 conn = redis.from_url(redis_url)
